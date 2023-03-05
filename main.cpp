b1c862c46d93ac8b6c3ecaef85fb677386b47382
3faze <93428357+3faze@users.noreply.github.com>
2023-03-05 19:37:48 +0000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    ifstream file;
+    string data;
+    cout << "File: " << argv[1] << endl;
+    file.open(argv[1]);
+    if (file.is_open())
+    {
+        char curr_char;
+        while (file)
+        {
+            curr_char = file.get();
+            data += curr_char;
+        }
+    }
+    file.close();
+    vector<string> lines;
+    string curr;
+    for (int i = 0; i < data.size(); i++)
+    {
+        if (data[i] != '\n')
+        {
+            curr += data[i];
+        }
+        else if (data[i] == '\n')
+        {
+            lines.push_back(curr);
+            curr = '\0';
+        }
+    }
+    for (int j = 0; j < lines.size(); j++)
+    {
+        cout << "[" << lines[j] << "] ";
+    }
+    return 0;
+}
